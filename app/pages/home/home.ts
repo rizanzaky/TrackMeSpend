96e6a75dfeb1c93f5df6649b9003b327cc0a54be
Rizan Zaky <rizan149@gmail.com>
2016-09-26 23:10:12 +0530

    home page initial

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController  } from 'ionic-angular';
 
 @Component({
-  templateUrl: 'build/pages/home/home.html'
+	templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {
-  
-  }
+	constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+
+	}
+
+	showPrompt() {
+		let prompt = this.alertCtrl.create({
+			title: 'Quick Record',
+			message: "Promptly add your income or expense for today, " + new Date().toLocaleDateString(),
+			inputs: [
+				{
+					name: 'type',
+					placeholder: 'Small description'
+				},
+				{
+					name: 'amount',
+					placeholder: 'Amount'
+				}
+			],
+			buttons: [
+				{
+					text: 'Cancel',
+					handler: data => {
+						console.log('Cancel clicked');
+					}
+				},
+				{
+					text: 'Save',
+					handler: data => {
+						console.log('Saved clicked');
+					}
+				}
+			]
+		});
+		prompt.present();
+	}
 }
